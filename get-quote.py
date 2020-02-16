ae750ac29026b22b4bb758cae23f7f24e6399dd0
friendbear <friendbear@users.noreply.github.com>
2020-02-17 08:31:06 +0900

    ramdom one line.

--- get-quote.py
+++ get-quote.py
@@ -2,15 +2,13 @@ import random
 
 def main():
 
-    last = 13
-    rnd = random.randint(0, last)
     print("Keep it logically awesome.")
 
     f = open("quotes.txt")
     quotes = f.readlines()
     f.close()
     
-    last = len(quotes) - 1
+    rnd = random.randint(0, len(quotes))
     print(quotes[rnd])
 
 if __name__== "__main__":
